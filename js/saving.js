b065d5f7e27c7e5d6bd9dff4158d13d1e9796130
Sielkewassenberg <56072944+Sielkewassenberg@users.noreply.github.com>
2023-06-18 23:07:32 +0200

    Update saving.js

--- js/saving.js
+++ js/saving.js
@@ -1,5 +1,5 @@
 "use strict";
-var a4 = [595.28, 841.89]; // 2de site for a4 size paper width and height
+var a4 = [841.89, 595.28]; // 2de site for a4 size paper width and height
 
 /* HTML-pagina wordt opgeslagen - div met id divID */
 function convertHTMLtoPDF() {
@@ -8,28 +8,24 @@ function convertHTMLtoPDF() {
     } = window.jspdf;
     var pdfjs = document.querySelector('#divID');
 
-    var doc = new jsPDF({
-        unit: 'px',
-        format: 'a4'
-    });
-
+  var doc = new jsPDF('l', 'mm', 'a4');
+    
     var options = {
         callback: function (doc) {
             doc.save("planner.pdf");
         },
-        x: 80, //positie van divID maar in units 1 units is 8 px
+        x: 20, //positie van divID maar in units 1 units is 8 px
         y: 0
     };
 
-    var contentHeight = pdfjs.offsetHeight;
-    var pageHeight = a4[1];
-    var scale = pageHeight / contentHeight;
+     var contentWidth = pdfjs.offsetWidth;
+    var pageWidth = a4[1];
+    var scale = pageWidth / contentWidth;
 
     // Controleer of de inhoud groter is dan één pagina en schaal het indien nodig
     if (contentHeight > pageHeight) {
         pdfjs.style.transformOrigin = 'top left';
-        pdfjs.style.marginRight = '15%';
-        pdfjs.style.transform = 'scale(0.5)';
+        pdfjs.style.transform = 'scale(0.35)';
         options.scale = scale;
     }
 
